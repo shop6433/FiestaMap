23c0936f860a8a91c181241f5c2df07053b3d42e
ParkSeonWoo820 <shop6433@naver.com>
2020-03-20 20:35:00 +0900

    임시파일

--- app/src/main/java/com/myfirstmapgoogle/fiestamap/MapsActivity.java
+++ app/src/main/java/com/myfirstmapgoogle/fiestamap/MapsActivity.java
@@ -185,16 +185,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (resultCode == 1) { }
         }
         else if (requestCode == 3){  //수정하기버튼을 눌러서 InfoEnterActivity 화면으로 넘어갔다가 온 정보
-            String name = data.getStringExtra("name");
-            String place = data.getStringExtra("place");
-            String time = data.getStringExtra("time");
-            String memo = data.getStringExtra("memo");
-            double Latitude = data.getDoubleExtra("Latitude",-1);
-            double Longitude = data.getDoubleExtra("Longitude",-1);
-            int order = data.getIntExtra("order",-1);
-            adjustMarker(Latitude,Longitude,name, place, memo, time,order);
-            adjustButton(name,order);
-
+            if(resultCode==RESULT_OK){
+                String name = data.getStringExtra("name");
+                String place = data.getStringExtra("place");
+                String time = data.getStringExtra("time");
+                String memo = data.getStringExtra("memo");
+                double Latitude = data.getDoubleExtra("Latitude", -1);
+                double Longitude = data.getDoubleExtra("Longitude", -1);
+                int order = data.getIntExtra("order", -1);
+                adjustMarker(Latitude, Longitude, name, place, memo, time, order);
+                adjustButton(name, order);
+            }
         }
 
     }
